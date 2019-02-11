6da30fc2fef1206f37a10ca9b92d08ee2fea95eb
David McDermott <crazyfox343@gmail.com>
2019-02-11 15:32:35 -0600

    Add project files.

---
+++ IntersectingChords/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntersectingChords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
